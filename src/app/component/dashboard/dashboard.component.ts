3390471b8b27358dc7c1404140e05fa79e943839
Wei Tian <twei@vmware.com>
2024-12-20 15:09:13 +0800

    first commit

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy, ViewChild } from "@angular/core";
-import { Subscription } from "rxjs";
+import { Subscription, timer } from "rxjs";
 import { GrafanaService } from "src/app/service/grafana.service";
 import { DashboardService } from "./dashboard.service";
 
@@ -13,10 +13,12 @@ export class DashboardComponent implements OnInit, OnDestroy {
   visualContainer: any;
 
   showTitle: boolean = false;
-  title: string;
+  title: string = "";
   yAxisTitle: string;
   fromDate: Date;
   toDate: Date;
+  refreshTime;
+  refreshMessage: string;
 
   selections = {
     metric: "",
@@ -34,9 +36,13 @@ export class DashboardComponent implements OnInit, OnDestroy {
         {
           label: "Memory Total",
           value: "m_total"
+        },
+        {
+          label: "Disk BPS",
+          value: "d_bps"
         }
       ],
-      selection: "",
+      selection: "d_bps",
       onChange: (field, value) => {
         this.selections[field] = value;
       }
@@ -47,7 +53,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
       selectValues: [
         {
           label: "Average",
-          value: "avergae"
+          value: "avg"
         },
         {
           label: "Max",
@@ -62,7 +68,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
           value: "sum"
         }
       ],
-      selection: "",
+      selection: "max",
       onChange: (field, value) => {
         this.selections[field] = value;
       }
@@ -74,70 +80,138 @@ export class DashboardComponent implements OnInit, OnDestroy {
     selectValues: [
       {
         label: "Last 24 hours",
-        value: 24 * 60 * 60 * 1000
+        value: "24h"
       },
       {
         label: "Last 2 days",
-        value: 2 * 24 * 60 * 60 * 1000
+        value: "2d"
       },
       {
         label: "Last 5 days",
-        value: 5 * 24 * 60 * 60 * 1000
+        value: "5d"
       },
       {
         label: "Last 7 days",
-        value: 7 * 24 * 60 * 60 * 1000
-      },
-      {
-        label: "Custom Range",
-        value: -1
+        value: "7d"
       }
+      // {
+      //   label: "Custom Range",
+      //   value: "custom"
+      // }
     ],
-    selection: 0
+    selection: "24h"
   };
+
+  copied = false;
 
   private subscriptions$: Subscription = new Subscription();
   private dashboardInfo;
+  private iframeUrl;
+  private iframeSnippet;
+
+  private refreshTimeSubscription$: Subscription;
 
   constructor(
     private grafanaService: GrafanaService,
     private dashboardService: DashboardService
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.initRefreshTimer();
+  }
 
   ngOnDestroy() {
     if (this.subscriptions$) {
       this.subscriptions$.unsubscribe();
     }
+    if (this.refreshTimeSubscription$) {
+      this.refreshTimeSubscription$.unsubscribe();
+    }
   }
 
   generateChart() {
+    this.iframeUrl = "";
     console.log("button clicked!");
     console.log(this.timeRange.selection);
 
     this.subscriptions$.add(
-      this.grafanaService.getDashboardInfo().subscribe((result: any) => {
-        console.log(result);
-        this.dashboardInfo = result;
-        if (
-          this.dashboardInfo &&
-          this.dashboardInfo.dashboard &&
-          this.dashboardInfo.dashboard.panels &&
-          this.dashboardInfo.dashboard.panels.length
-        ) {
-          const snippet = this.dashboardService.generateIframeSnippet(
-            this.dashboardInfo.dashboard.title,
-            this.dashboardInfo.dashboard.panels[0].id
-          );
-          console.log(snippet);
-          this.visualContainer.nativeElement.innerHTML = snippet;
-        }
-      })
+      this.grafanaService
+        .getChartIframe(
+          this.options[0].selection,
+          this.options[1].selection,
+          this.title,
+          this.timeRange.selection
+        )
+        .subscribe(
+          (iframeUrl: any) => {
+            console.log(iframeUrl);
+            this.iframeUrl = iframeUrl;
+            this.iframeSnippet = this.grafanaService.generateIFrameWithTimeStamp(
+              this.iframeUrl,
+              this.timeRange.selection
+            );
+            this.refreshTime = Date.now();
+            this.visualContainer.nativeElement.innerHTML = this.iframeSnippet;
+          },
+          (error: any) => {
+            console.log("error when generating grafana chart.");
+            console.log(error);
+          }
+        )
     );
+  }
+
+  reloadIFrame() {
+    if (this.iframeUrl) {
+      console.log("reload iframe");
+      this.iframeSnippet = this.grafanaService.generateIFrameWithTimeStamp(
+        this.iframeUrl,
+        this.timeRange.selection
+      );
+      this.visualContainer.nativeElement.innerHTML = this.iframeSnippet;
+      this.refreshTime = Date.now();
+    }
+  }
+
+  copyIFrame() {
+    if (this.iframeSnippet) {
+      navigator.clipboard.writeText(this.iframeSnippet).then(
+        () => {
+          console.log("Copying the clipboard was successful!");
+          this.copied = true;
+        },
+        err => {
+          console.error("Could not copy text: ", err);
+          this.copied = false;
+        }
+      );
+    }
+  }
+
+  onCopyMsgClose() {
+    this.copied = false;
   }
 
   showDateTimePicker() {
     return Number(this.timeRange.selection) === -1;
+  }
+
+  initRefreshTimer() {
+    const refreshTimeObservable = timer(0, 5000);
+    this.refreshTimeSubscription$ = refreshTimeObservable.subscribe(() => {
+      if (!this.refreshTime) {
+        return;
+      }
+      const interval: number = Math.floor(
+        Math.abs(Date.now() - this.refreshTime) / 60 / 1000
+      );
+      if (interval === 0) {
+        this.refreshMessage = "Refreshed in less than 1 minute.";
+      } else if (interval === 1) {
+        this.refreshMessage = `Refresh in ${interval} min ago.`;
+      } else {
+        this.refreshMessage = `Refresh in ${interval} mins ago.`;
+      }
+    });
   }
 }
--- src/app/service/grafana.service.ts
+++ src/app/service/grafana.service.ts
@@ -8,15 +8,64 @@ import {
 } from "@angular/common/http";
 import { map } from "rxjs/operators";
 
+const INTERVAL_UNIT_MAP = {
+  h: 60 * 60 * 1000,
+  d: 24 * 60 * 60 * 1000
+};
+
 @Injectable({
   providedIn: "root"
 })
 export class GrafanaService {
   constructor(private http: HttpClient) {}
 
-  getDashboardInfo() {
+  getChartIframe(metricField, metricType, title, interval) {
+    let from, to;
+    if (interval && interval !== "custom") {
+      from = `now-${interval}`;
+      to = "now";
+    }
     return this.http
-      .get("http://10.111.24.229:3000/api/dashboards/uid/kztSL9aZk")
-      .pipe(map(resp => resp));
+      .post(
+        `https://demo-group10101-sunny.devframe.cp.horizon.vmware.com/demo-base/v1/panels`,
+        {
+          metricsfield: metricField,
+          metricstype: metricType,
+          from: from,
+          to: to,
+          title: title
+        },
+        {
+          responseType: "text",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+          }
+        }
+      )
+      .pipe(
+        map(iframeUrl => {
+          return iframeUrl;
+        })
+      );
+    // return this.http
+    //   .get("http://10.111.24.229:3000/api/dashboards/uid/kztSL9aZk")
+    //   .pipe(map(resp => resp));
+  }
+
+  generateIFrameWithTimeStamp(iframeUrl, interval) {
+    let toInMs = Date.now();
+    let fromInMs = toInMs - this.parseIntervalToMilliseconds(interval);
+    return `<iframe src="${iframeUrl}&from=${fromInMs}&to=${toInMs}" width="100%" height="100%" frameborder="0"></iframe>`;
+  }
+
+  private parseIntervalToMilliseconds(interval: string) {
+    const unitKeys = Object.keys(INTERVAL_UNIT_MAP);
+    for (let i = 0; i < unitKeys.length; i++) {
+      if (interval.indexOf(unitKeys[i]) !== -1) {
+        let intervalInNum = Number(interval.split(unitKeys[i])[0]);
+        return intervalInNum * INTERVAL_UNIT_MAP[unitKeys[i]];
+      }
+    }
   }
 }
